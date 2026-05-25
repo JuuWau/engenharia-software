574d6a7daaa2ba4d5d917b7548ec85143ac3e213
Júlia Wauters <juliawauters04@gmail.com>
2026-05-25 18:59:33 -0300

    feat: created occurrence routes

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,22 @@
 
 use App\Http\Controllers\OccurrencesController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::inertia('/', 'Welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
+Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
 Route::middleware(['auth', 'verified'])->prefix('occurrences')->name('occurrences.')->group(function() {
-    Route::get('/', [OccurrencesController::class, 'index'])->name('index');
-    Route::post('/', [OccurrencesController::class, 'store'])->name('store');
+    Route::get('/', [OccurrencesController::class, 'index'])->name('occurrences.index');
+    Route::post('/', [OccurrencesController::class, 'store'])->name('occurrences.store');
+    Route::put('/{occurrence}', [OccurrencesController::class, 'update'])->name('occurrences.update');
+    Route::delete('/{occurrence}', [OccurrencesController::class, 'destroy'])->name('occurrences.destroy');
 });
 
 require __DIR__.'/settings.php';
