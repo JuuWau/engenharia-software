886f963f245048ec1291c9ab3eac360f5d6b2fcb
Júlia Wauters <juliawauters04@gmail.com>
2026-06-16 20:14:21 -0300

    feat: created diary components

---
+++ resources/js/pages/diary/components/header/DiaryHeader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ChevronLeft, ChevronRight } from 'lucide-vue-next';
+
+defineProps<{
+  currentMonthLabel: string
+}>()
+
+defineEmits<{
+  previous: []
+  next: []
+  today: []
+}>()
+</script>
+
+<template>
+  <div class="w-full max-w-5xl flex justify-between items-center mb-6 pt-10">
+    <div>
+      <h1 class="text-2xl font-semibold sm:text-3xl">
+        Diário
+      </h1>
+
+      <p class="mt-1 text-sm text-muted-foreground sm:text-base">
+        Registre e acompanhe anotações diárias com segurança.
+      </p>
+    </div>
+
+    <div class="flex items-center gap-3">
+      <button @click="$emit('previous')" class="hover:">
+        <ChevronLeft />
+      </button>
+
+      <span>
+        {{ currentMonthLabel }}
+      </span>
+
+      
+      <button @click="$emit('next')">
+        <ChevronRight />
+      </button>
+
+      <button @click="$emit('today')">
+        Hoje
+      </button>
+    </div>
+  </div>
+</template>
